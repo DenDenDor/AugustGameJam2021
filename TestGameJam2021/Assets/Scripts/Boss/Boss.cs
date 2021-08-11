61dc9a3db28a3a4155a5e2f827c7a284ed8baf3b
DenDenDor <dendor333@gmail.com>
2021-08-11 21:47:26 +0300

    cool

---
+++ TestGameJam2021/Assets/Scripts/Boss/Boss.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Boss : MonoBehaviour
+{
+   
+    [SerializeField] private int _damage;
+    [SerializeField] private int _health;
+    [SerializeField] private int _speed;
+    [SerializeField] private Player _character;
+
+    public int Damage { get => _damage; set => _damage = value; }
+    public int Health { get => _health; set => _health = value; }
+    public Player Character { get => _character; set => _character = value; }
+    public int Speed { get => _speed; set => _speed = value; }
+
+    public abstract void Attack();
+    public abstract void Move();
+    public abstract void Die();
+}
--- TestGameJam2021/Assets/Scripts/Player/Player.cs
+++ TestGameJam2021/Assets/Scripts/Player/Player.cs
@@ -10,15 +10,15 @@ public class Player : MonoBehaviour
    [SerializeField] private float _range;
    private bool _isTurned;
    private Vector3 _sizes;
+   private int _extra;
    private void Start() {
        _sizes = transform.localScale;
    }
    private void Update() {
        float movement = Input.GetAxis("Horizontal");
-       
-       _rigidbody.velocity = new Vector2(movement,0) * _speed;
+       _rigidbody.velocity = new Vector2(movement* _speed,_rigidbody.velocity.y);
        bool _isOnEarth = Physics2D.OverlapCircle(_checkPoint.position,_range, _layer);
-       Debug.Log(_isOnEarth);
+       
        if (Input.GetKeyDown(KeyCode.Space) && _isOnEarth ==true)
        {
           Jump();
@@ -33,7 +33,10 @@ public class Player : MonoBehaviour
 
 
    }
-   private void Jump() => _rigidbody.velocity = Vector2.up*_jumpforse;
+   private void Jump() 
+   { 
+       _rigidbody.velocity = Vector2.up*_jumpforse;
+   }
    
    private void ChangeDirection() {
        float _turn = _isTurned ?_sizes.x : -_sizes.x;
